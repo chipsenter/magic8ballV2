baf22f575fb59aabd3f788d4efd951f58631550c
Johan <jlindstrom@lifetimefitness.com>
2017-11-25 20:25:04 -0700

    Initial Commit

---
+++ Magic 8 Ball 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball 2
+//
+//  Created by Johan Lindstrom on 11/25/17.
+//  Copyright © 2017 Johan Lindstrom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
