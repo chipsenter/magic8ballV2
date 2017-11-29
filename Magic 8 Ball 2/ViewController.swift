393e5e370d3a254e180630e681e93687649cdb83
Johan <jlindstrom@lifetimefitness.com>
2017-11-28 23:12:33 -0700

    Final Project

--- Magic 8 Ball 2/ViewController.swift
+++ Magic 8 Ball 2/ViewController.swift
@@ -10,16 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let ballArray = ["ball1", "ball2", "ball3", "ball4", "ball5", "ball6", "ball7", "ball8", "ball9", "ball10", "ball11", "ball12", "ball13"]
+    
+    var randomBallNumber : Int = 0
+  
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+      newBallImage()
+        
+        
+    }
+    
+    func newBallImage() {
+        
+        randomBallNumber = Int(arc4random_uniform(13))
+        
+        imageView.image = UIImage(named: ballArray[randomBallNumber])
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    @IBAction func askButtonPressed(_ sender: Any) {
+        
+        newBallImage()
+        
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        
+        newBallImage()
+    }
+    
 }
 
---
+++ Magic 8 Ball 2Tests/Magic_8_Ball_2Tests.swift
@@ -0,0 +1,35 @@
+//
+//  Magic_8_Ball_2Tests.swift
+//  Magic 8 Ball 2Tests
+//
+//  Created by Johan Lindstrom on 11/27/17.
+//  Copyright © 2017 Johan Lindstrom. All rights reserved.
+//
+
+import XCTest
+
+class Magic_8_Ball_2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
